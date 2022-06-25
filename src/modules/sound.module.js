0ee47d2fa53a9fc40aacf0be74b3b282a3f97e07
Timur <jesorstaff@gmail.com>
2022-06-25 17:00:55 +0500

    create sound module

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -1,3 +1,17 @@
 import { Module } from "../code/module";
+import { random } from "../utils";
 
-export class Sound extends Module {}
+export class Sound extends Module {
+  constructor(type, text) {
+    super("sound", "Случайный звук");
+  }
+
+  trigger() {
+    const audio = document.createElement("audio");
+    audio.src = `assets/audio/sound${random(1, 26)}.mp3`;
+    document.body.append(audio);
+
+    audio.currentTime = 0;
+    audio.play();
+  }
+}
